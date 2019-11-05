2c91a5974f98a5b8b70de395eaa3940c80013294
Celia Sherry <sherry.c@husky.neu.edu>
2019-11-04 20:32:32 -0500

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="container-fluid">
+                <h1>
+                    Home Page
+                </h1>
+            </div>
+        )
+    }
+}
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Home from './components/Home';
+
+ReactDOM.render(
+    <div className="container-fluid">
+        <Home/>
+    </div>,
+    document.getElementById('root')
+);
+
