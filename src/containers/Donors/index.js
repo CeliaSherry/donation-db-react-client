8f60ea2694110aa6980dbc8e24746ce429d8b914
Khazana <>
2019-11-17 17:01:01 -0500

    tweaks

--- src/containers/Donors/index.js
+++ src/containers/Donors/index.js
@@ -24,8 +24,6 @@ export class Donors extends Component {
 
         <div style={{ display: "flex", justifyContent: "center" }}>
           <Card style={{ width: "80em" }}>
-            <br></br>
-            <br></br>
             <DonorList />
           </Card>
         </div>
