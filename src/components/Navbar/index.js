8417d3d91de910148b2408ccb331e7e68cbe0fec
Khazana <>
2019-11-16 21:54:09 -0500

    adding get donations from donor

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,7 +4,6 @@ import Button from "react-bootstrap/Button";
 import Navbar from "react-bootstrap/Navbar";
 import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
-import style from "./Navbar.css";
 import FormControl from "react-bootstrap/FormControl";
 
 export default class NavbarCustom extends Component {
@@ -16,9 +15,8 @@ export default class NavbarCustom extends Component {
                 <Navbar bg="dark" variant="dark">
                     <Navbar.Brand href="/donations">Lake County United</Navbar.Brand>
                     <Nav className="mr-auto">
-                        {/* <Nav.Link  exact activeClassName={style.activeLink} href="/donations">Donations</Nav.Link> */}
-                        <Nav.Link activeClassName="active" href="/donors">Donors</Nav.Link>
-                        <Nav.Link activeClassName="active" href="/institutions">Institutions</Nav.Link>
+                        <Nav.Link href="/donors">Donors</Nav.Link>
+                        <Nav.Link href="/institutions">Institutions</Nav.Link>
                     </Nav>
                     <Form inline>
                         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
