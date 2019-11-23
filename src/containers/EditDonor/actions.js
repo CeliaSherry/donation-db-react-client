cbf5f1b82395a8a9cedeac6bac8588deaab68f39
mlevoy <54318929+mlevoy@users.noreply.github.com>
2019-11-23 14:41:39 -0500

    update donor now saving
    -update donor form now saves info
    -scroll to top and give success or error message on submit
    -still need to figure out institution and zip

--- src/containers/EditDonor/actions.js
+++ src/containers/EditDonor/actions.js
@@ -10,3 +10,27 @@ export function getDonor(donorId){
         },
     };
 }
+
+export function updateDonor(donorId, donorName,email,phone,address,state,city,zip,institution){
+    return{
+        [RSAA]:{
+            types: ['REQUEST', 'SUCCESS', 'FAILURE'],
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            endpoint: `http://localhost:8080/api/donor/${donorId}`,
+            body:    JSON.stringify({
+                donorName: donorName,
+                email: email,
+                phone: phone,
+                address: address,
+                state: state,
+                city: city,
+                zip:zip,
+                institution: institution,
+            }),
+            method:'PUT',
+        },
+    };
+}
--- src/containers/EditDonor/index.js
+++ src/containers/EditDonor/index.js
@@ -8,16 +8,20 @@ import * as actions from "./actions";
 import FormControl from "react-bootstrap/FormControl";
 import {bindActionCreators} from "redux";
 import {connect} from "react-redux";
+import Alert from "react-bootstrap/Alert";
 
+//TODO get zipcode update and institution get and update to work!  Rerender list and forms on back button
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({
     getDonor: actions.getDonor,
+    updateDonor: actions.updateDonor,
   }, dispatch);
 }
 
 export class EditDonor extends Component {
   constructor(props) {
     super(props)
+    this.myRef = React.createRef();
     this.state = {
       name: "",
       phone: "",
@@ -28,6 +32,7 @@ export class EditDonor extends Component {
       zipCode: "",
       institution: ""
     }
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -39,7 +44,8 @@ export class EditDonor extends Component {
         address: response.payload.address,
         city: response.payload.city,
         addrState: response.payload.state,
-        zipCode: response.payload.zipCode
+        zipCode: response.payload.zipCode,
+        institution: response.payload.institution
       })
     })
   }
@@ -50,138 +56,165 @@ export class EditDonor extends Component {
   //   return this.email.length > 0 && this.password.length > 0;
   // }
 
-  handleSubmit(event) {
-    event.preventDefault();
+
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, phone, address, addrState, city, zipCode, institution } = this.state;
+    const { updateDonor } = this.props;
+    updateDonor(this.props.match.params.donorId, name, email, phone, address, addrState, city, zipCode, institution).then(response => {
+      this.setState({ submitted: true })
+      if (response.type === 'SUCCESS') {
+        this.setState({ success: true })
+      }
+      if (response.type === 'FAILURE') {
+        this.setState({ success: false })
+      }
+      setTimeout(() => {
+        this.setState({ submitted: false });
+      }, 3000);
+    });
+    window.scrollTo(0, this.myRef.current.top);
   }
 
   render() {
     return (
+        <div>
+            <div ref={this.myRef}/>
+          <div style={{display: "flex", justifyContent: "center"}}>
+            {this.state.success && this.state.submitted ?
+                <Alert isOpen={this.state.visible} style={{width: "48rem"}} variant='success'> Successful donor
+                  update!</Alert>
+                : !this.state.success && this.state.submitted ?
+                    <Alert style={{width: "48rem"}} variant='danger'> Error!</Alert> : ''}
+          </div>
+            <div style={{display: "flex", justifyContent: "center"}}>
+              <Card style={{width: "48rem"}}>
+                <Card.Header as="h5">Edit Donor</Card.Header>
+                <Card.Body>
+                  <div className="Edit Donor">
+                    <form onSubmit={this.handleSubmit}>
+                      <Row>
+                        <Col>
+                          <label htmlFor="inputFullName">Full Name</label>
+                          {(this.state.name != null) ?
+                              <FormControl value={this.state.name}
+                                           onChange={e => this.setState({name: e.target.value})}
+                                           type="text"/>
+                              : <FormControl value={this.name}
+                                             onChange={e => this.setState({name: e.target.value})}
+                                             type="text"/>}
+                        </Col>
+                      </Row>
+                      <br></br>
+                      <FormGroup controlId="email" bsSize="large">
+                        <label htmlFor="inputEmail">Email Address</label>
+                        {(this.state.email != null) ?
+                            <FormControl value={this.state.email}
+                                         onChange={e => this.setState({email: e.target.value})}
+                                         type="text"/>
+                            : <FormControl value={this.email}
+                                           onChange={e => this.setState({email: e.target.value})}
+                                           type="text"/>}
+                      </FormGroup>
+                      <br></br>
+                      <label htmlFor="inputPhone">Phone Number</label>
+                      <FormGroup controlId="phone" bsSize="large">
+                        {(this.state.phone != null) ?
+                            <FormControl value={this.state.phone}
+                                         onChange={e => this.setState({phone: e.target.value})}
+                                         type="text"/>
+                            : <FormControl value={this.phone}
+                                           onChange={e => this.setState({phone: e.target.value})}
+                                           type="text"/>}
+                      </FormGroup>
+                      <br></br>
+                      <FormGroup controlId="address" bsSize="large">
+                        <label htmlFor="inputStreet">Street Address</label>
+                        {(this.state.address != null) ?
+                            <FormControl value={this.state.address}
+                                         onChange={e => this.setState({address: e.target.value})}
+                                         type="text"/>
+                            : <FormControl value={this.address}
+                                           onChange={e => this.setState({address: e.target.value})}
+                                           type="text"/>}
+                      </FormGroup>
+                      <br></br>
+                      <Row>
+                        <Col>
+                          <FormGroup controlId="city" bsSize="large">
+                            <label htmlFor="inputCity">City</label>
+                            {(this.state.city != null) ?
+                                <FormControl value={this.state.city}
+                                             onChange={e => this.setState({city: e.target.value})}
+                                             type="text"/>
+                                : <FormControl value={this.city}
+                                               onChange={e => this.setState({city: e.target.value})}
+                                               type="text"/>}
+                          </FormGroup>
+                        </Col>
+                        <Col>
+                          <FormGroup controlId="state" bsSize="large">
+                            <label htmlFor="inputState">State</label>
+                            {(this.state.addrState != null) ?
+                                <FormControl value={this.state.addrState}
+                                             onChange={e => this.setState({addrState: e.target.value})}
+                                             type="text"/>
+                                : <FormControl value={this.addrState}
+                                               onChange={e => this.setState({addrState: e.target.value})}
+                                               type="text"/>}
+                          </FormGroup>
+                        </Col>
+                        <Col>
+                          <FormGroup controlId="zip" bsSize="large">
+                            <label htmlFor="inputZip">Zip</label>
+                            {(this.state.zipCode != null) ?
+                                <FormControl value={this.state.zipCode}
+                                             onChange={e => this.setState({zipCode: e.target.value})}
+                                             type="text"/>
+                                : <FormControl value={this.zipCode}
+                                               onChange={e => this.setState({zipCode: e.target.value})}
+                                               type="text"/>}
+                          </FormGroup>
+                        </Col>
+                      </Row>
+                      <br></br>
+                      <FormGroup controlId="institution" bsSize="large">
+                        <label htmlFor="inputInstitution">Institution</label>
+                        {(this.state.institution != null) ?
+                            <FormControl value={this.state.institution}
+                                         onChange={e => this.setState({institution: e.target.value})}
+                                         type="text"/>
+                            : <FormControl value={this.institution}
+                                           onChange={e => this.setState({institution: e.target.value})}
+                                           type="text"/>}
+                      </FormGroup>
 
-        <div style={{display: "flex", justifyContent: "center"}}>
-          <Card style={{width: "48rem"}}>
-            <Card.Header as="h5">Edit Donor</Card.Header>
-            <Card.Body>
-              <div className="Edit Donor">
-                <form onSubmit={this.handleSubmit}>
-                  <Row>
-                    <Col>
-                      <label htmlFor="inputFullName">Full Name</label>
-                      {(this.state.name != null) ?
-                          <FormControl value={this.state.name}
-                                       onChange={e => this.setState({name: e.target.value})}
-                                       type="text"/>
-                          : <FormControl value={this.name}
-                                         onChange={e => this.setState({name: e.target.value})}
-                                         type="text"/>}
-                    </Col>
-                  </Row>
-                  <br></br>
-                  <FormGroup controlId="email" bsSize="large">
-                    <label htmlFor="inputEmail">Email Address</label>
-                    {(this.state.email != null) ?
-                        <FormControl value={this.state.email}
-                                     onChange={e => this.setState({email: e.target.value})}
-                                     type="text"/>
-                        : <FormControl value={this.email}
-                                       onChange={e => this.setState({email: e.target.value})}
-                                       type="text"/>}
-                  </FormGroup>
-                  <br></br>
-                  <label htmlFor="inputPhone">Phone Number</label>
-                  <FormGroup controlId="phone" bsSize="large">
-                    {(this.state.phone != null) ?
-                        <FormControl value={this.state.phone}
-                                     onChange={e => this.setState({phone: e.target.value})}
-                                     type="text"/>
-                        : <FormControl value={this.phone}
-                                       onChange={e => this.setState({phone: e.target.value})}
-                                       type="text"/>}
-                  </FormGroup>
-                  <br></br>
-                  <FormGroup controlId="address" bsSize="large">
-                    <label htmlFor="inputStreet">Street Address</label>
-                    {(this.state.address != null) ?
-                        <FormControl value={this.state.address}
-                                     onChange={e => this.setState({address: e.target.value})}
-                                     type="text"/>
-                        : <FormControl value={this.address}
-                                       onChange={e => this.setState({address: e.target.value})}
-                                       type="text"/>}
-                  </FormGroup>
-                  <br></br>
-                  <Row>
-                    <Col>
-                      <FormGroup controlId="city" bsSize="large">
-                        <label htmlFor="inputCity">City</label>
-                        {(this.state.city != null) ?
-                            <FormControl value={this.state.city}
-                                         onChange={e => this.setState({city: e.target.value})}
-                                         type="text"/>
-                            : <FormControl value={this.city}
-                                           onChange={e => this.setState({city: e.target.value})}
-                                           type="text"/>}
-                      </FormGroup>
-                    </Col>
-                    <Col>
-                      <FormGroup controlId="state" bsSize="large">
-                        <label htmlFor="inputState">State</label>
-                        {(this.state.addrState != null) ?
-                            <FormControl value={this.state.addrState}
-                                         onChange={e => this.setState({addrState: e.target.value})}
-                                         type="text"/>
-                            : <FormControl value={this.addrState}
-                                           onChange={e => this.setState({addrState: e.target.value})}
-                                           type="text"/>}
-                      </FormGroup>
-                    </Col>
-                    <Col>
-                      <FormGroup controlId="zip" bsSize="large">
-                        <label htmlFor="inputZip">Zip</label>
-                        {(this.state.zipCode != null) ?
-                            <FormControl value={this.state.zipCode}
-                                         onChange={e => this.setState({zipCode: e.target.value})}
-                                         type="text"/>
-                            : <FormControl value={this.zipCode}
-                                           onChange={e => this.setState({zipCode: e.target.value})}
-                                           type="text"/>}
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                  <br></br>
-                  <FormGroup controlId="institution" bsSize="large">
-                    <label htmlFor="inputInstitution">Institution</label>
-                    {(this.state.institution != null) ?
-                        <FormControl value={this.state.institution}
-                                     onChange={e => this.setState({institution: e.target.value})}
-                                     type="text"/>
-                        : <FormControl value={this.city}
-                                       onChange={e => this.setState({institution: e.target.value})}
-                                       type="text"/>}
-                  </FormGroup>
-                  <br></br>
-                  <Button
-                      block
-                      bsSize="large"
-                      // disabled={!this.validateForm()}
-                      type="submit"
-                  >
-                    Save
-                  </Button>
-                  <Button
-                      block
-                      bsSize="large"
-                      href={`/donors`}
-                      // disabled={!this.validateForm()}
-                      type="submit"
-                  >
-                    Cancel
-                  </Button>
-                </form>
-              </div>
-            </Card.Body>
-          </Card>
-        </div>
-    );
+                      <br></br>
+                      <Button
+                          block
+                          bsSize="large"
+                          // disabled={!this.validateForm()}
+                          type="submit"
+                      >
+                        Save
+                      </Button>
+                      <Button
+                          block
+                          bsSize="large"
+                          href={`/donors`}
+                          // disabled={!this.validateForm()}
+                          type="submit"
+                      >
+                        Cancel
+                      </Button>
+                    </form>
+                  </div>
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+          );
   }
 }
 
